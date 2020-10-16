cc1979cfa0824c26177bd5894744bd9879994b48
Khiemaily <49074638+Khiemaily@users.noreply.github.com>
2020-10-15 19:22:53 -0700

    Add files via upload
    add find recurring character in a string

---
+++ FindRecurringCharFromString.ps1
@@ -0,0 +1,15 @@
+﻿function get-recurringchar {
+   [CmdletBinding()]
+   param (
+     [string]$inputstring
+   )
+  
+   $inputstring.ToCharArray() |
+   Group-Object -NoElement |
+   where Count -gt 1 |
+   where Name -eq 'd'
+   Sort-Object -Property Count -Descending
+
+}
+get-recurringchar 'abcdedcba'
+
